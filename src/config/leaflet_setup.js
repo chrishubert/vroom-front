11650f56e2c01f690a6c3c190b4a77764ff0ca59
Christophe Hubert <hubert.christophe@gmail.com>
2021-09-12 14:17:42 +0700

    Improve base

--- src/config/leaflet_setup.js
+++ src/config/leaflet_setup.js
@@ -5,7 +5,7 @@ var api = require('./api');
 
 L.Icon.Default.imagePath = 'css/images/';
 
-var initCenter = L.latLng(48.8579,2.3494);
+var initCenter = L.latLng(-6.200000,106.816666);
 var initZoom = 13;
 
 var attribution = 'Routes computed using <a href="http://project-osrm.org/">OSRM</a>'
