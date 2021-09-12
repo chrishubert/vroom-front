70131fb89bb5ade28362da05d7ad85450fc984d3
Christophe Hubert <hubert.christophe@gmail.com>
2021-09-12 14:15:10 +0700

    Improve base

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-  host: process.env.VROOM_PORT,
-  port: process.env.VROOM_HOST,
+  host: 'http://157.245.145.81', // TODO: Use .env File
+  port: '3000', // TODO: Use .env File
   maxTaskNumber: 1000,
   overpassEndpoint: 'https://overpass-api.de/api/interpreter'
 };
